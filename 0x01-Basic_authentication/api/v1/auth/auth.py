72ef2e7180f8da3aaedff209ec2c51e2e9e9a99c
Robert Odhiambo <odhizrober254@gmail.com>
2024-05-24 20:54:23 +0300

    authentication class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ Auth class Module """
 from flask import request
+from typing import List, TypeVar
+import fnmatch
 
 
 class Auth():
@@ -9,8 +11,18 @@ class Auth():
             self,
             path: str,
             excluded_paths: List[str]) -> bool:
-        """ returns False - path and excluded_paths """
-        return False
+        """ checks if auth is required """
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ returns None """
