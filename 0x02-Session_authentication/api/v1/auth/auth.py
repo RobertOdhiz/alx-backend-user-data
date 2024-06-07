129068ad03c596af55258af4cb8128d433cfa5d2
Robert Odhiambo <odhizrober254@gmail.com>
2024-06-07 14:56:05 +0300

    Et moi et moi et moi!

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Auth class Module """
+from flask import request
+from typing import List, TypeVar
+import fnmatch
+
+
+class Auth():
+    """ Authentication Class representation """
+    def require_auth(
+            self,
+            path: str,
+            excluded_paths: List[str]) -> bool:
+        """ checks if auth is required """
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ returns None """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ returns None """
+        return None
